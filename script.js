b935fc992c6990ff8e06bf40eacc5cc008f634b7
Danila Ioan-Alexandru <15651883+MortalROs@users.noreply.github.com>
2023-02-24 17:40:09 +0200

    gg

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ function togglemenu() {
 
     if(menuList.style.maxHeight == "0px")
         menuList.style.maxHeight = "400px";
-        else
+        else {
         menuList.style.maxHeight = "0px";
+        menuList.style.margin = "0px;"
+        }
 }
